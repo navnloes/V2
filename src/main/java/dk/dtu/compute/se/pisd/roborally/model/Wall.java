bf3be72b35585dd229ce9835c81f41272a0b7bb9
Mia <miadong179@gmail.com>
2021-03-09 16:22:29 +0100

    wall constructor

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Wall.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Wall.java
@@ -4,4 +4,18 @@ import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
 
 public class Wall extends Subject {
 
+    public final Board board;
+    public final int x;
+    public final int y;
+    private boolean hit;
+    public final Heading heading;
+
+    public Wall(Board board, int x, int y, Heading heading){
+        this.board = board;
+        this.x = x;
+        this.y = y;
+        this.heading = heading;
+        hit = false;
+    }
+
 }
