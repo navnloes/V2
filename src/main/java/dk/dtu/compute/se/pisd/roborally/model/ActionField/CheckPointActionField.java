99040ac639aad3ac4db8ff62ed6a8a0a0c809ead
Christian Pedersen <chrpedersen861@gmail.com>
2021-03-21 12:25:26 +0100

    Java doc til Actionfield klasser.

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/ActionField/CheckPointActionField.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/ActionField/CheckPointActionField.java
@@ -5,7 +5,15 @@ import dk.dtu.compute.se.pisd.roborally.model.Board;
 public class CheckPointActionField extends ActionField {
 
     int id;
-
+    /**
+     * @author S201192
+     *  This method construtcs a gearsactionfield.
+     * @param board
+     * @param x x is the coordinats of the space.
+     * @param y y is the coordinates of the space.
+     * @param msg sends a msg to the plauer.
+     * @param id is the unique ID for each checkpoint
+     */
     public CheckPointActionField(Board board, int x, int y, String msg, int id){
         super(board, x, y, msg);
         this.id = id;
