7c6858490524e28d6acf11ef05295d6a056b4430
Mia <miadong179@gmail.com>
2021-04-14 22:43:00 +0200

    JavaDoc i CheckPointFieldAction

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/CheckPointFieldAction.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/CheckPointFieldAction.java
@@ -3,18 +3,41 @@ package dk.dtu.compute.se.pisd.roborally.controller;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
+/**
+ * @author s205353
+ * This class is the specialized FieldAction class for CheckPointsActions
+ */
+
 public class CheckPointFieldAction extends FieldAction{
 
+    /**
+     * Each CheckPoint has an unique ID
+     */
     private int checkPointId;
 
+    /**
+     * This method is used in order to fetch the ID of a given checkPoint
+     * @return checkPointId : int
+     */
     public int getCheckPointId() {
         return checkPointId;
     }
 
+    /**
+     * This method is a setter in order to set the CheckPoint ID of a given CheckPoint
+     * @param checkPointId checkPointId : int
+     */
     public void setCheckPointId(int checkPointId) {
         this.checkPointId = checkPointId;
     }
 
+    /**
+     *
+     * @param gameController the gameController of the respective game
+     * @param space the space this action should be executed for
+     * @return boolean true if the action is executed and false, if the action isn't executed
+     */
+    @Override
     public boolean doAction(GameController gameController, Space space) {
         Player player = space.getPlayer();
         if (player != null){
