71d3005909e7dc29ace4b6477a9195da4f932986
Mia <miadong179@gmail.com>
2021-04-16 01:15:09 +0200

    Laser virker med playerAction

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/LaserPlayerAction.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/LaserPlayerAction.java
@@ -4,19 +4,29 @@ import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
+/**
+ * @author s205353, s205339, s201192
+ */
 public class LaserPlayerAction extends PlayerAction {
 
+    /**
+     * This integer defines the range, that the robots can shoot with their laser
+     */
     private int range = 3;
 
+    /**
+     * This method models the action of the Laser
+     * @param gameController the gameController of the respective game
+     * @param player the player whose action should be executed
+     * @return boolean true if the action is performed right
+     */
     @Override
     public boolean doAction(GameController gameController, Player player) {
-
         if (player != null){
             Space space = player.getSpace();
             Heading heading = player.getHeading();
 
             boolean robotInRange = false;
-
             Player otherRobot = null;
 
             for (int i = 0; i < range; i++) {
@@ -28,7 +38,6 @@ public class LaserPlayerAction extends PlayerAction {
                     break;
                 }
             }
-
             if (robotInRange) {
                 otherRobot.hit();
                 if (otherRobot.isReboot()) {
@@ -36,11 +45,9 @@ public class LaserPlayerAction extends PlayerAction {
                     otherRobot.setReboot(false);
                 }
             }
-
             return true;
         } else {
             return false;
         }
-
     }
 }
